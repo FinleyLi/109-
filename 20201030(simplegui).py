ba649e2dec6db812053eed8217bfd604236c9e3e
Finley <galoislee@yahoo.com.tw>
2020-11-18 15:56:35 +0800

    Create 20201030(simplegui).py

---
+++ 20201030(simplegui).py
@@ -0,0 +1,27 @@
+# ver. 10/30
+# 視窗輸入數值資料
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 09:02:01 2020
+
+@author: user
+"""
+!pip install PySimpleGUI
+import PySimpleGUI as sg                        # Part 1 - The import
+
+# Define the window's contents
+layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
+            [sg.Input()],
+            [sg.Button('Ok')] ]
+
+# Create the window
+window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
+
+# Display and interact with the Window
+event, values = window.read()                   # Part 4 - Event loop or Window.read call
+
+# Do something with the information gathered
+print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+
+# Finish up by removing from the screen
+window.close()                                  # Part 5 - Close the Window
