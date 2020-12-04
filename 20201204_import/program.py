0ea639870f80c644bc56812d05e9a078c6338b44
FinleyLi <galoislee@yahoo.com.tw>
2020-12-04 23:48:03 +0800

    add inspect can see souce code

--- 20201204_import/program.py
+++ 20201204_import/program.py
@@ -1,16 +1,20 @@
 import myClass as mc
 from myClass import multi
 import whatIsMyIp as ip
+import inspect
 
 def minus(num1, num2):
-	minu = num1 - num2
-	return minu
+	minued = num1 - num2
+	return minued
 
 def main():
+	'''
 	print(mc.adder(3, 5))
 	print(f'multi: {multi(20, 30)}')
 	print('minus: ', minus(51, 1))
 	ip.main()
+	'''
+	print(inspect.getsource(mc.adder))
 
 if __name__ == '__main__' :
 	main()
