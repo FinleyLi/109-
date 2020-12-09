398937261af35479c0fc16bec8c2a0964c619b7b
Finley <galoislee@yahoo.com.tw>
2020-12-09 16:46:26 +0800

    加入隨機等待避免每日簽到時間皆相同

--- 20201211_ChromeDriver/GettingStarted.py
+++ 20201211_ChromeDriver/GettingStarted.py
@@ -8,50 +8,43 @@ This is a temporary script file.
 #pip install selenium
 
 import time
+import random # 隨機數
 from selenium import webdriver
-
-# private mode
-'''
-from selenium.webdriver.chrome.options import Options
-
-
-options = Options()
-#關閉瀏覽器跳出訊息
-prefs = {
-    'profile.default_content_setting_values' :
-        {
-        'notifications' : 2
-         }
-}
-options.add_experimental_option('prefs',prefs)
-options.add_argument("--headless")            #不開啟實體瀏覽器背景執行
-options.add_argument("--start-maximized")     #最大化視窗
-options.add_argument("--incognito")           #開啟無痕模式
-'''
-
 from selenium.webdriver.common.keys import Keys
 
+# 隨機數
+def dice():
+	return random.randint(1, 120) # 至多延遲120秒
+
+# 開啟校務系統差勤
 Browser = webdriver.Chrome()
 LoginUrl= ('https://softwork.tsh.tp.edu.tw/t-tools/index.php')
 UserName= ('hk3345678')
 UserPass= ('ilikemoney')
+time.sleep(dice()) # 設定等待登入時間每日皆不同
 
+# 登入差勤打卡簽到
 Browser.get(LoginUrl)
 Browser.find_element_by_id('teacher_log').send_keys(UserName)
 Browser.find_element_by_id('teacher_pw').send_keys(UserPass)
 Browser.find_element_by_id('teacher_pw').send_keys(Keys.ENTER)
 time.sleep(5) # Let the user actually see something!
 '''
+# 校務系統使用jQuery Mobile無按鈕可選
 btn = Browser.find_element_by_name("Submit")
 btn.click()
 '''
+# 進入"簽到退(專任)"
 dailycheckURL = ('https://softwork.tsh.tp.edu.tw/t-tools/dailycheck.php')
 Browser.get(dailycheckURL)
 Browser.save_screenshot('dailycheck.png')
 
+# 簽退
 dailycheckoutURL = ('https://softwork.tsh.tp.edu.tw/t-tools/dailycheck_show.php?action=checkout')
 Browser.get(dailycheckoutURL)
 Browser.save_screenshot('dailycheckout.png')
-Browser.refresh()
+Browser.refresh() # 重新整理網頁確認成功
 time.sleep(5) # Let the user actually see something!
+
+# 關閉Chrome
 Browser.quit()
