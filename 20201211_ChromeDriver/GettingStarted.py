5ab5ebd54d51994c487f2204fe21a1b58be0f059
Finley <galoislee@yahoo.com.tw>
2020-12-09 11:33:59 +0800

    Wrong demo auto Daily check

--- 20201211_ChromeDriver/GettingStarted.py
+++ 20201211_ChromeDriver/GettingStarted.py
@@ -10,11 +10,48 @@ This is a temporary script file.
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/');
+# private mode
+'''
+from selenium.webdriver.chrome.options import Options
+
+
+options = Options()
+#關閉瀏覽器跳出訊息
+prefs = {
+    'profile.default_content_setting_values' :
+        {
+        'notifications' : 2
+         }
+}
+options.add_experimental_option('prefs',prefs)
+options.add_argument("--headless")            #不開啟實體瀏覽器背景執行
+options.add_argument("--start-maximized")     #最大化視窗
+options.add_argument("--incognito")           #開啟無痕模式
+'''
+
+from selenium.webdriver.common.keys import Keys
+
+Browser = webdriver.Chrome()
+LoginUrl= ('https://softwork.tsh.tp.edu.tw/t-tools/index.php')
+UserName= ('hk3345678')
+UserPass= ('ilikemoney')
+
+Browser.get(LoginUrl)
+Browser.find_element_by_id('teacher_log').send_keys(UserName)
+Browser.find_element_by_id('teacher_pw').send_keys(UserPass)
+Browser.find_element_by_id('teacher_pw').send_keys(Keys.ENTER)
 time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
+'''
+btn = Browser.find_element_by_name("Submit")
+btn.click()
+'''
+dailycheckURL = ('https://softwork.tsh.tp.edu.tw/t-tools/dailycheck.php')
+Browser.get(dailycheckURL)
+Browser.save_screenshot('dailycheck.png')
+
+dailycheckoutURL = ('https://softwork.tsh.tp.edu.tw/t-tools/dailycheck_show.php?action=checkout')
+Browser.get(dailycheckoutURL)
+Browser.save_screenshot('dailycheckout.png')
+Browser.refresh()
 time.sleep(5) # Let the user actually see something!
-driver.quit()
+Browser.quit()
